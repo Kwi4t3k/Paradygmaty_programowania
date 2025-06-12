e9cac09430179d1907cff265a3b279e41cdf23f9
Kinga <153843750+Kwi4t3k@users.noreply.github.com>
2025-06-12 09:57:14 +0200

    Update zad5.hs

--- Haskell/12 Haskell, część 2/zad5.hs
+++ Haskell/12 Haskell, część 2/zad5.hs
@@ -8,3 +8,18 @@ colors = ["black", "white", "blue", "yellow", "red"]
 func :: [String] -> [(String, String)]
 func [x] = []
 func (first:rest) = [(first, x) | x<-rest] ++ func rest
+
+
+
+main :: IO()                                 -- główna akcja programu w IO
+main = do
+    print (func colors)                      -- wywołaj func na liście colors i wypisz wynik
+
+colors = ["black", "white", "blue", "yellow", "red"]
+                                             -- definicja listy kolorów
+
+func :: [String] -> [(String, String)]       -- sygnatura: func zamienia listę Stringów na listę par
+func [x] = []                                -- przypadek bazowy: pojedynczy element → brak par
+func (first:rest) =                          -- gdy lista ma head i tail...
+    [(first, x) | x <- rest]                --   utwórz wszystkie pary (first, każdy element z rest)
+    ++ func rest                             --   dodaj rekurencyjnie wynik dla rest
