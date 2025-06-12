73804f18531b142dec892b9a3326464ec8849c43
Kinga <153843750+Kwi4t3k@users.noreply.github.com>
2025-06-12 09:55:13 +0200

    Update zad3.hs

--- Haskell/12 Haskell, część 2/zad3.hs
+++ Haskell/12 Haskell, część 2/zad3.hs
@@ -1,12 +1,13 @@
 -- Napisz funkcję, która pobiera listę i zwraca tę samą listę z elementami w odwróconej kolejności, nie korzystając z gotowej funkcji reverse.
 
-main :: IO()
+main :: IO()                     -- główna akcja programu typu IO
 main = do
-    print (rev [1,2,3])
+    print (rev [1,2,3])         -- wywołaj rev na [1,2,3] i wypisz efekt
 
-rev :: [a] -> [a]
-rev x = rev' x []
+rev :: [a] -> [a]                -- sygnatura: rev przyjmuje listę a i zwraca listę a
+rev x = rev' x []                -- wywołaj pomocnicze rev' z x jako wejściem i pustym akumulatorem
 
-rev' :: [a] -> [a] -> [a]
-rev' [] y = y
-rev' (first:rest) y = rev' rest (first:y)
+rev' :: [a] -> [a] -> [a]         -- sygnatura: rev' bierze listę do odwrócenia i akumulator, zwraca wynik
+rev' [] y = y                    -- jeżeli nie ma już elementów (wejście puste), rezultat to akumulator
+rev' (first:rest) y =            -- w przeciwnym razie rozbij listę na head=first i tail=rest
+    rev' rest (first:y)          -- dodaj pierwszy element na początek akumulatora i rekurencyjnie przetwarzaj resztę
